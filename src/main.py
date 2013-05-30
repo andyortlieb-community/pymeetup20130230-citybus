694ae166797ccd0b45efa77fa6aba9a13aa9c6cd
Ortlieb <andy.ortlieb@it12-0297.local>
2013-05-30 14:14:05 -0500

    Version endpoint

--- src/main.py
+++ src/main.py
@@ -1,16 +1,32 @@
 from flask import Flask
 from jinja2 import Template
+import json
 
 from datetime import datetime
 
 app = Flask(__name__)
 app.debug = True
 
+class Constants:
+	Version = '0.0.2'
+	SupportedVersions = [ '0.0.1','0.0.2' ]
+
+
 @app.route('/')
 def home():
 	return Template(open('./templates/home.html', 'r').read()).render({
 		'date': datetime.now(),
 		'buscount': 32
+	})
+
+@app.route('/version/')
+def version():
+	return json.dumps({ 
+		'Success':True,
+		'Data': {
+			'Version':Constants.Version,
+			'SupportedVersions':Constants.SupportedVersions
+		}
 	})
 
 if __name__=='__main__':
