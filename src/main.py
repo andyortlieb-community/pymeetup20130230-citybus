942a67883160bbb07ca2a1c24226de3f769294e5
Ortlieb <andy.ortlieb@it12-0297.local>
2013-05-30 14:01:45 -0500

    Template

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from jinja2 import Template
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def home():
-	return "Welcome to Citybus"
+	return Template(open('./templates/home.html', 'r').read()).render()
 
 if __name__=='__main__':
 	app.run()
