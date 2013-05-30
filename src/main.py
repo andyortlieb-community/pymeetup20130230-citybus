81ef151c93ce3a17e1e899c5bca258771227dcca
Ortlieb <andy.ortlieb@it12-0297.local>
2013-05-30 14:07:02 -0500

    Cooler templates

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from jinja2 import Template
 
+from datetime import datetime
+
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
 def home():
-	return Template(open('./templates/home.html', 'r').read()).render()
+	return Template(open('./templates/home.html', 'r').read()).render({
+		'date': datetime.now(),
+		'buscount': 32
+	})
 
 if __name__=='__main__':
 	app.run()
