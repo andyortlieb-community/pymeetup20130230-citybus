815fd2970ba43a9102ceca6007fce0141d6e3166
Ortlieb <andy.ortlieb@it12-0297.local>
2013-05-30 15:13:50 -0500

    Busroute endpoint uses database

--- src/main.py
+++ src/main.py
@@ -1,16 +1,49 @@
+from sqlalchemy.ext.declarative import declarative_base
 from flask import Flask
 from jinja2 import Template
 import json
 
 from datetime import datetime
 
-app = Flask(__name__)
-app.debug = True
-
 class Constants:
 	Version = '0.0.2'
 	SupportedVersions = [ '0.0.1','0.0.2' ]
 
+###
+### 	The Database...
+###
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy import Column,Integer,String
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("sqlite:///./citybus.db")
+#engine = create_engine("sqlite:///:memory:")
+metadata = MetaData()
+Model = declarative_base(metadata=metadata)
+Model.toDict = lambda r: {c.name: getattr(r, c.name) for c in r.__table__.columns}
+session = sessionmaker()
+session.configure(bind=engine)
+session = session()
+
+###
+###	Database Models
+###
+
+class BusRoute(Model):
+	__tablename__ = "bus_route"
+	id = Column(String(5), primary_key=True)
+	busses = Column(Integer)
+
+
+
+####
+####	The flask app ....
+####
+
+app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def home():
@@ -31,18 +64,43 @@ def version():
 
 @app.route('/v1/busroutes', methods=['GET'])
 def busroutes():
+	routes = [ route.toDict() for route in session.query(BusRoute).all() ]
 	return json.dumps({
 		'Success':True,
-		'Data':[
-			{ 
-				'route':'201',
-				'busses':17
-			},{
-				'route':'211',
-				'busses':30
-			}
-		]
+		'Data': routes
 	})
 
+def SetupDB():
+	# Delete the db file
+	import os
+	try:
+		os.unlink('citybus.db')
+	except OSError:
+		pass # It's okay if the file doesn't exist. 
+
+	# Create our database, add the models, etc.
+ 	metadata.create_all(engine)		
+
+	# Add a couple bus routes
+	bus201 = BusRoute()
+	bus201.id = '201'
+	bus201.busses = 17
+
+	bus211 = BusRoute()
+	bus211.id = '221'
+	bus211.busses = 30
+
+	bus315 = BusRoute()
+	bus315.id = '315'
+	bus315.busses = 12
+	
+	session.add(bus201)
+	session.add(bus211)
+	session.add(bus315)
+	#session.commit()
+
 if __name__=='__main__':
+	SetupDB()
+	
+	# Start up the web app
 	app.run()
